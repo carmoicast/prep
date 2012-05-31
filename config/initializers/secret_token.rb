9f78056ba0aad449b1e59f1ebdcf7bfab9e09543
Carlos Moises <carmoicast@gmail.com>
2012-05-31 16:26:03 -0500

    primer commit sin devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Encuentas::Application.config.secret_token = 'e28f3b2e6a2c66109152f342ff11a26e5d6c1c990c3d93b01c31db4d014763f0599fdd05e267d495dc916d68be789c3eee92dcb257fa47fac0697c9b9171268a'
