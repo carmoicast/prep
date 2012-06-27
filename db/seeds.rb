766bd3240c0e570cb97ce8386628e4f506ea5143
Carlos Castro <carmoicast@gmail.com>
2012-06-27 10:38:51 -0500

    seed para cargar el layout de las casillas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+def lowercase_accents(string)
+  string.gsub('Á','á').gsub('É','é').gsub('Í','í').gsub('Ó','ó').gsub('Ú','ú') if string
+end
+
+#DISTRITO	CASILLA	MUNICIPIO	DOMICILIO	USER_ID
+
+puts "Leyendo archivo"
+casillas = File.read('/Users/carlitos/Desktop/Casillas_por_municipio_prep.csv').force_encoding('MacRoman').encode('UTF-8')
+puts "Parseando archivo"
+casillas_array = CSV.parse(casillas, :headers => true)
+casillas_array.each do |casilla|
+  
+#distrito = State.find_or_create_by_name(casilla['DISTRITO'])
+
+puts "Creando casilla #{casilla['DISTRITO']} #{casilla['CASILLA']} #{casilla['MUNICIPIO']} #{casilla['DOMICILIO']} #{casilla['USER_ID']} \n"
+# DISTRITO,CASILLA,MUNICIPIO,DOMICILIO
+
+  casilla = Box.create( distrito: casilla['DISTRITO'],
+    casilla: casilla['CASILLA'],
+    municipio: casilla['MUNICIPIO'],
+    direccion: casilla['DOMICILIO'],
+    user_id: casilla['USER_ID']  )
+
+#  casilla.create_result
+end
